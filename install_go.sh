f329729bded12019418b3c775c2483c65c98a1ac
oldforum <47047172+oldforum@users.noreply.github.com>
2023-11-15 02:23:23 +0800

    Update install_go.sh

--- install_go.sh
+++ install_go.sh
@@ -8,12 +8,9 @@ sudo apt remove --autoremove golang-go
 sudo rm -rf /usr/local/go
 
 # 获取最新版本的 Go 二进制文件
-GO_LATEST=$(curl -s https://golang.org/VERSION?m=text)
+GO_LATEST=$(curl -s https://go.dev/VERSION?m=text | grep -oP 'go[0-9.]+')
 if [[ $GO_LATEST == go* ]]; then
     GO_URL="https://dl.google.com/go/${GO_LATEST}.linux-amd64.tar.gz"
-
-    # 打印出下载 URL
-    echo "下载 URL: $GO_URL"
 
     # 下载并解压 Go
     curl -L $GO_URL | sudo tar -C /usr/local -xzf -
